4d77ad169f09b20e4629f30baf3087cd8399df30
Naman Jain <162738854+NamanjainAkt@users.noreply.github.com>
2025-06-22 00:07:24 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -33,7 +33,7 @@ function ProfilePage() {
   }
 
   return (
-    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center backdrop-blur-xl'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
        
         <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
