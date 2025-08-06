cb9a7ca7272193ed0d9ef43acd4751e7b1cca4ad
Naman Jain <162738854+NamanjainAkt@users.noreply.github.com>
2025-08-06 21:49:29 +0530

    Update utils.js

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -3,6 +3,6 @@ import jwt from "jsonwebtoken"
 
 //function to generate token for a user
 export const generateToken = (userId)=>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET)
+    const token = jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn: "15d"})
     return token
 }
